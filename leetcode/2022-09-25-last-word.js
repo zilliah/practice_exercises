5d00b60e6cbbc435adb59b9baeafcec2cbd946ac
Kerry Smart <kerry.z.smart@gmail.com>
2022-09-25 18:45:53 -0400

    fix for mltiple whitespace

--- leetcode/2022-09-25-last-word.js
+++ leetcode/2022-09-25-last-word.js
@@ -2,7 +2,7 @@
  * @param {string} s
  * @return {number}
  */
-var lengthOfLastWord = function(s) {
-    const a = s.split(" ");
+ var lengthOfLastWord = function(s) {
+    const a = s.trim().split(/\s+/);
     return a[a.length -1 ].length;
 };
